8edaed04fe1bd677ffb04b0a02ead1d6310306aa
Morteza Sabihi <mortezasabihi87@gmail.com>
2020-06-15 15:40:38 +0430

    Cors added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const consola = require("consola");
 const chalk = require("chalk");
 const key = require("./config/key");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 app.use(require("./routes/index"));
 
 app.listen(key.port, () =>
